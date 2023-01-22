7c2dca85d7ec032bdca90f4c859a8f26c7e582cd
Bob Pham <bobbaopham@gmail.com>
2023-01-21 16:38:45 -0800

    added stuff for frontend mainpage

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -1,5 +1,6 @@
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import { useState } from 'react';
+import { checkPropTypes } from 'prop-types';
 
 const Dictaphone = () => {
     const {
@@ -14,27 +15,39 @@ const Dictaphone = () => {
     }
   
     return (
-      <div>
-        <p>Microphone: {listening ? 'on' : 'off'}</p>
-        <button onClick={SpeechRecognition.startListening}>Start</button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
-        <p>{transcript}</p>
+      <div className="grid place-items-center">
+        <div className="bg-white text-black rounded-lg p-10 my-5">
+          <p>{transcript}</p>
+        </div>
+        <p className="text-center">Microphone: {listening ? 'on' : 'off'}</p>
+        <div>
+          <button className="btn btn-success mx-5" onClick={SpeechRecognition.startListening}>Start</button>
+          <button className="btn btn-error mx-5" onClick={SpeechRecognition.stopListening}>Stop</button>
+          <button className="btn btn-warning mx-5" onClick={resetTranscript}>Reset</button>
+        </div>
       </div>
     );
   };
 
-const Response = () => {
-    // TODO Create response for May
-    return null;
+const Response = (props) => {
+    return (
+      <div className={props.resp ? "bg-white text-black rounded-lg p-10 my-5" : "bg-blue-600"}>
+      <p>{props.resp}</p>
+      </div>
+    );
 }
 
 export default function MainPage() {
+    const [resp, setResp] = useState("")
+
     return (
         <main className="bg-blue-500 w-screen h-screen grid place-items-center">
-        <div className="text-2xl text-white">Let's Talk!</div>
+          <h1 className="text-6xl text-white">Let's Talk!</h1>
+        <div className="text-2xl text-white">
             <Dictaphone />
-            <Response />
+            <Response resp={resp}/>
+        </div>
+        <button className="btn bg-white text-black" onClick={() => setResp("This is random ass text")}>ahhhhh</button>
         </main>
     )
 }
