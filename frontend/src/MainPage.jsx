cf5514c38adea0d837747f51914db584c8d70f8b
brianberger <brianberger23@outlook.com>
2023-01-22 09:39:22 -0800

    reset mainpage to transcribe rather than record

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -34,114 +34,130 @@ to the console never shows up, and the status never changes from queued, as far 
 const assembly = axios.create({
   baseURL: "https://api.assemblyai.com/v2",
   headers: {
-    authorization: "key",           //I have an API key but they say to keep it private and not commit it or anything
+    authorization: "123a7d1e21d34b8ab2eebd38982e4c1f",           
+    //I have an API key but they say to keep it private and not commit it or anything
     //there's a whole section like "more on that later" but they don't actually touch on it once after they initially bring it up
     //if you need it I'll just send it to you in discord if I'm awake but like the shit's free if you need to get your own
     //that's probably easier 
     "content-type": "application/json",
-    "transfer-encoding":"chunked",      //TODO: this line is the problem child I think 
+    // "transfer-encoding":"chunked",      //TODO: this line is the problem child I think 
   },
 })
 
 const Dictaphone = () => {
 
-  const recorder = useRef(null);
-  const audioPlayer = useRef(null) 
-  const [blobURL, setBlobUrl] = useState(null)
-  const [audioFile, setAudioFile] = useState(null)
-  const [isRecording, setIsRecording] = useState(null)
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
 
-  useEffect(() => {
-    recorder.current = new MicRecorder({ bitRate: 128 })
-  }, [])
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  // const recorder = useRef(null);
+  // const audioPlayer = useRef(null) 
+  // const [blobURL, setBlobUrl] = useState(null)
+  // const [audioFile, setAudioFile] = useState(null)
+  // const [isRecording, setIsRecording] = useState(null)
+
+  // useEffect(() => {
+  //   recorder.current = new MicRecorder({ bitRate: 128 })
+  // }, [])
 
 
   const startRecording = () => {
-    recorder.current.start().then(() => {
-      setIsRecording(true)
-    })
+    SpeechRecognition.startListening();
+    // recorder.current.start().then(() => {
+    //   setIsRecording(true)
+    // })
   }
 
 
   const stopRecording = () => {
-    recorder.current
-      .stop()
-      .getMp3()
-      .then(([buffer, blob]) => {
-        const file = new File(buffer, "audio.mp3", {
-          type: blob.type,
-          lastModified: Date.now(),
-        })
-        const newBlobUrl = URL.createObjectURL(blob)
-        setBlobUrl(newBlobUrl) //sets source for audio controller - I'm assuming this is what we need to use
-        //if we try and make it go to the backend 
-        setIsRecording(false)
-        setAudioFile(file) //stores it in the state variable, I guess. 
-      })
-      .catch((e) => console.log(e))
+    SpeechRecognition.stopListening();
+    // recorder.current
+    //   .stop()
+    //   .getMp3()
+    //   .then(([buffer, blob]) => {
+    //     const file = new File(buffer, "audio.mp3", {
+    //       type: blob.type,
+    //       lastModified: Date.now(),
+    //     })
+    //     const newBlobUrl = URL.createObjectURL(blob)
+    //     setBlobUrl(newBlobUrl) //sets source for audio controller - I'm assuming this is what we need to use
+    //     //if we try and make it go to the backend 
+    //     setIsRecording(false)
+    //     setAudioFile(file) //stores it in the state variable, I guess. 
+    //   })
+    //   .catch((e) => console.log(e))
   }
   
-  const [uploadURL, setUploadURL] = useState("")
-  const [transcriptID, setTranscriptID] = useState("")
-  const [transcriptData, setTranscriptData] = useState("")
-  const [transcript, setTranscript] = useState("")
+  // const [uploadURL, setUploadURL] = useState("")
+  // const [transcriptID, setTranscriptID] = useState("")
+  // const [transcriptData, setTranscriptData] = useState("")
+  // const [transcript, setTranscript] = useState("")
 
     // Submit the Upload URL to AssemblyAI and retrieve the Transcript ID
-    const submitTranscriptionHandler = () => {
-      assembly
-        .post("/transcript", {
-          audio_url: uploadURL,
-        })
-        .then((res) => {
-          setTranscriptID(res.data.id)
-        })
-        .catch((err) => console.error(err))
-    }
+    // const submitTranscriptionHandler = () => {
+    //   assembly
+    //     .post("/transcript", {
+    //       audio_url: uploadURL,
+    //     })
+    //     .then((res) => {
+    //       setTranscriptID(res.data.id)
+    //     })
+    //     .catch((err) => console.error(err))
+    // }
 
     /*
     Reason we do it like this instead of just having it like automatically check the status is I guess it's easier to debug
     this way? Honestly a little confused here but like I'm confused everywhere. 
     */
-    const checkStatusHandler = async () => {
-      try {
-        await assembly.get(`/transcript/${transcriptID}`).then((res) => {
-          setTranscriptData(res.data)
-      setTranscript(transcriptData.text)
-        })
-      } catch (err) {
-        console.error(err)
-      }
-    }
+    // const checkStatusHandler = async () => {
+    //   try {
+    //     await assembly.get(`/transcript/${transcriptID}`).then((res) => {
+    //       setTranscriptData(res.data)
+    //   setTranscript(transcriptData.text)
+    //     })
+    //   } catch (err) {
+    //     console.error(err)
+    //   }
+    // }
 
-  // Upload the Audio File and retrieve the Upload URL
-  useEffect(() => {
-    if (audioFile) {
-      assembly
-        .post("/upload", audioFile)
-        .then((res) => setUploadURL(res.data.upload_url))
-        .catch((err) => console.error(err))
-    }
-  }, [audioFile])
+  // // Upload the Audio File and retrieve the Upload URL
+  // useEffect(() => {
+  //   if (audioFile) {
+  //     assembly
+  //       .post("/upload", audioFile)
+  //       .then((res) => setUploadURL(res.data.upload_url))
+  //       .catch((err) => console.error(err))
+  //   }
+  // }, [audioFile])
 
-  //this part works, actually. A little too well even. Usually the uploadURL gets printed to the console twice.
-  //Not really sure why. 
-  console.log(uploadURL)
+  // //this part works, actually. A little too well even. Usually the uploadURL gets printed to the console twice.
+  // //Not really sure why. 
+  // console.log(uploadURL)
   
   //We really don't need the audio player but it's a good sanity check to make sure the audio is recording
     return (
       <div>
         <div place-items-center>
+        <div className="grid place-items-center">
         <div className="bg-white text-black rounded-lg p-10 my-5">
+          <p>{transcript}</p>
         </div>
-        <audio ref={audioPlayer} src={blobURL} controls='controls' />         
-          <button className="btn btn-success m-5" disabled={isRecording} onClick={startRecording}>
+        <p className="text-center">Microphone: {listening ? 'on' : 'off'}</p>
+        {/* <audio ref={audioPlayer} src={blobURL} controls='controls' />          */}
+          <button className="btn btn-success m-5"  onClick={startRecording}>
           Start
           </button>
-          <button className="btn btn-error m-5" disabled ={!isRecording} onClick={stopRecording}>
+          <button className="btn btn-error m-5" onClick={stopRecording}>
             Stop</button>
-          <button className="btn btn-warning m-5" onClick={null}>Reset</button>
-          <button onClick={submitTranscriptionHandler}>SUBMIT</button>
-          <button onClick={checkStatusHandler}>CHECK STATUS</button>
+          <button className="btn btn-warning m-5" onClick={resetTranscript}>Reset</button>
+        </div>
         </div>
         </div>
     );
@@ -180,4 +196,10 @@ export default function MainPage({name}) {
         </main>
     )
 }
+
+/**
+ * VISIBLE PROBLEMS:
+ * All the text to speech stuff goes twice every single page and it's incredibly annoying 
+ * 
+ */
 
