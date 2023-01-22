ae407a52ea97314091d88ad0ca5b632e9b01ca97
brianberger <brianberger23@outlook.com>
2023-01-21 18:47:07 -0800

    records audio instead of live transcription

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -30,7 +30,6 @@ const Dictaphone = () => {
           });
           }}>Start</button>
           <button className="btn btn-error mx-5" onClick={() => {
-          SpeechRecognition.stopListening
           Mp3Recorder.stop().getMp3().then(([buffer, blob]) => {
             const file = new File(buffer, "input.mp3", {
               type: blob.type,
